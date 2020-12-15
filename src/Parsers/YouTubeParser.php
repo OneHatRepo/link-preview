a81bc87767b0aaf48c31f53597b6275b9f830486
Scott Spuler <scott@onehat.com>
2020-12-15 14:14:07 -0600

    Tweak embed code

--- src/Parsers/YouTubeParser.php
+++ src/Parsers/YouTubeParser.php
@@ -55,7 +55,7 @@ class YouTubeParser extends BaseParser implements ParserInterface
         $this->getPreview()
             ->setId($matches[1])
             ->setEmbed(
-                '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$this->getPreview()->getId().'?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+                '<iframe src="https://www.youtube.com/embed/'.$this->getPreview()->getId().'?controls=0" width="100%" height="100%" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
             );
 
         return $this;
